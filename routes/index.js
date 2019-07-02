928cb4a501bc110d02cdd032bac505d067862c48
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-02 16:03:12 -0500

    set up basic login route

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,20 @@ router.get('/about', function(req, res, next) {
   res.send('hi this is sakura :D'); 
 });
 
-
 router.post('/', function(req, res, next) {
   res.send('Got a POST request'); 
 });
+
+router.post('/login', function(req, res, next) {
+	var username = req.body.username; 
+	var password = req.body.password; 
+	if(username == "Sakura" && password == "123") {
+		res.send({loginStatus: "success"});
+	} else {
+		res.send({loginStatus: "failure"}); 
+	}
+});
+
 
 
 module.exports = router;
