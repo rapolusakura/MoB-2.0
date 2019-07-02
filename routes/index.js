283dae8ed278e15c43698b99e352f60b1b11f63b
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-02 15:20:51 -0500

    can make requests

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.send('hi this is sakura :D'); 
+});
+
+
+router.post('/', function(req, res, next) {
+  res.send('Got a POST request'); 
+});
+
+
 module.exports = router;
