b3a62fc2095e4691ab46f29b7291be931990a4d2
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-04 13:25:58 -0500

    installed cors

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentWillMount() {
+      this.callAPI();
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and savekehaksdfh to reload.
           </p>
           <a
             className="App-link"
