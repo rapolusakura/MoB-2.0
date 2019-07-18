6f8c80d56e93db5d295fcb21d743c3efc9be72a4
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 10:32:57 -0500

    fixed

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,65 +1,6 @@
 import React from 'react'; 
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 
-<<<<<<< HEAD
-export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username : '', 
-      password : '', 
-    }
-  }
-
-    mySubmitHandler = (event) => {
-    event.preventDefault();
-
-    fetch("http://localhost:9000/login", {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: this.state.username, 
-        password: this.state.password,
-      }),
-    }).then(response => response.text())
-      .then(text => 
-        alert(text));  
-  }
-
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    this.setState({[nam]: val});
-  }
-
-    render() {
-    return (
-      <form onSubmit={this.mySubmitHandler}>
-        <p>Enter your email:</p>
-        <input
-        type='text'
-        name='username'
-        onChange={this.myChangeHandler}
-      />
-        <p>Enter your password:</p>
-      <input
-        type='text'
-        name='password'
-        onChange={this.myChangeHandler}
-      />
-      <br/> 
-      <br/>
-      <input
-        type='submit'
-      />
-      </form>
-    )
-  }
-}
-=======
 const Login = () => (
   <Formik 
     
@@ -84,4 +25,3 @@ onSubmit={values => {
 )
 
 export default Login; 
->>>>>>> master
