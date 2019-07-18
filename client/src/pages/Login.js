4db277ce5450bf6344c9d40037cbe4ab1539cb51
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 11:31:00 -0500

    created sign up form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -14,6 +14,11 @@ const SignupSchema = Yup.object().shape({
   email: Yup.string()
     .email('Invalid email')
     .required('Required'),
+  password: Yup.string()
+    .min(6, 'Too short!')
+    .required('Password is required'),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
 });
 
 
@@ -31,16 +36,21 @@ const Login = () => (
     {({ touched, errors }) => (
     <Form>
         <div> 
-          <Field name="firstName" />
+          <Field name="firstName" placeholder="First Name" />
           {errors.firstName && touched.firstName ? (
             <div>{errors.firstName}</div>
           ) : null}
-          <Field name="lastName" />
+          <Field name="lastName" placeholder = "Last Name" />
           {errors.lastName && touched.lastName ? (
             <div>{errors.lastName}</div>
           ) : null}
-          <Field name="email" type="email" />
+          <Field name="email" type="email" placeholder = "Email" />
           {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <Field name="password" type="password" placeholder = "Enter password" />
+          {errors.password && touched.password ? <div>{errors.password}</div> : null}
+
+          <Field name="confirmPassword" type="password" placeholder = "Confirm password" />
+          {errors.confirmPassword && touched.confirmPassword ? <div>{errors.confirmPassword}</div> : null}
           <button type="submit">Submit</button>
         </div> 
     </Form>
