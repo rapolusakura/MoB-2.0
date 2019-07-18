612e69971177abb11f621d5cefacca46fccf037f
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 11:22:04 -0500

    basic signup schema with yup

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,38 +1,49 @@
 import React from 'react'; 
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  lastName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+});
+
 
 const Login = () => (
   <Formik 
-    
-initialValues={{ name: '' }}
-
-validate={values => {
-  let errors = {};
-  if(!values.name) {
-    errors.name = 'Name is required';
-  }
-  return errors;
-}}
-    
-onSubmit={values => {
-      console.log('submitting', values);
+    initialValues={{
+        firstName: '',
+        lastName: '',
+        email: ''
+      }}
+    validationSchema={SignupSchema}
+    onSubmit={values => {
+          console.log('submitting', values);
     }}>
-    {({ handleSubmit, handleChange, values, touched, errors }) => (
-    <form onSubmit={handleSubmit}>
-    <div> 
-      <input 
-             onChange={handleChange} 
-             value={values.name}
-             name = "name"
-             type="text" 
-             placeholder="fuck">
-      </input>
-      <button>Submit</button>
-      {errors.name && touched.name && <span style={{ color:"red", fontWeight: "bold" }}>
-    {errors.name}    
-    </span> }
-      </div> 
-    </form>
+    {({ touched, errors }) => (
+    <Form>
+        <div> 
+          <Field name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <Field name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <Field name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <button type="submit">Submit</button>
+        </div> 
+    </Form>
     )}
   </Formik>
 )
