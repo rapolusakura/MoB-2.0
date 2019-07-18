b78c8a2f3bf5b4f7f2845fafe7b4fff7c2cf72bf
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 16:57:13 -0500

    valid sign in/sign out

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('../db.js'); 
 const Order = require('../models/orders.js'); 
 const User = require('../models/User');
+const UserSession = require('../models/UserSession'); 
 
 //get home page
 router.get('/', function(req, res, next) {
@@ -129,7 +130,57 @@ router.post('/signup', (req, res, next) => {
         });
       });
     });
-  }); // end of sign up endpoint
+});
 
+router.post('/signin', (req, res, next) => {
+	const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+    email = email.toLowerCase().trim(); 
+    User.find({
+      email: email
+    }, (err, users) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+      const user = users[0];
+      if (!user.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+      // Otherwise correct user
+      const userSession = new UserSession();
+      userSession.userId = user._id;
+      userSession.save((err, doc) => {
+        if (err) {
+          console.log(err);
+          return res.send({
+            success: false,
+            message: 'Error: server error'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Valid sign in',
+          token: doc._id
+        });
+      });
+    });
+}); 
 
 module.exports = router;
