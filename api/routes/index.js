9505b3989f1231e07f27b13c8d1db121d0417a0d
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 12:16:34 -0500

    orders work again

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db.js'); 
 var Order = require('../models/orders.js'); 
-require('./orders.js'); 
 
 //get home page
 router.get('/', function(req, res, next) {
@@ -21,6 +21,59 @@ router.post('/login', function(req, res, next) {
 		res.send({loginStatus: "failure"}); 
 	}
 });
+
+router.post('/createOrder', function(req, res, next) {
+	var company_name = req.body.company_name; 
+	var rate = req.body.rate; 
+	var delivery_status = req.body.delivery_status; 
+
+	const order = new Order({ 
+		company_name: company_name, 
+		rate: rate, 
+		date_created: Date.now(),
+		delivery_status: delivery_status
+	}); 
+
+	order.save(function (err, order) {
+		if(err) return console.error(err); 
+		else {
+			res.send("Order has successfully been created!"); 
+		}
+	});
+});
+
+router.get('/getOutgoingOrders', function(req, res, next) {
+	Order.find({ "delivery_status": "outgoing" }, function(err, orders) {
+        if (err) {
+            console.log(err);
+
+        } else {
+          res.send(orders);  
+        }  
+    })
+}); 
+
+router.get('/getPendingOrders', function(req, res, next) {
+	Order.find({ "delivery_status": "pending" }, function(err, orders) {
+        if (err) {
+            console.log(err);
+
+        } else {
+          res.send(orders);  
+        }  
+    })
+}); 
+
+router.get('/getCompletedOrders', function(req, res, next) {
+	Order.find({ "delivery_status" : "completed" }, function(err, orders) {
+        if (err) {
+            console.log(err);
+
+        } else {
+          res.send(orders);  
+        }  
+    })
+}); 
 
 
 module.exports = router;
