b5715ddf3c1e15f42719b26f11dfab9b0297bf69
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-18 10:31:40 -0500

    formik gives response

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 const Login = () => (
-  <Formik>
-  //do formik markup here
-  </Formik> 
+  <Formik 
+    
+initialValues={{ name: '' }}
+    
+onSubmit={values => {
+      console.log('submitting', values);
+    }}>
+    {({ handleSubmit, handleChange, values }) => (
+    <form onSubmit={handleSubmit}>
+      <input 
+             onChange={handleChange} 
+             value={values.name}
+             name = "name"
+             type="text" 
+             placeholder="fuck">
+      </input>
+      <button>Submit</button>
+    </form>
+    )}
+  </Formik>
 )
 
 export default Login; 
