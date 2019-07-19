0b2b13fbf424bfedbfddf061262b6f84a184335f
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-19 10:55:35 -0500

    cookie is properly set

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,10 @@
 import React from 'react'; 
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
+import {
+  setInStorage,
+  getFromStorage,
+} from '../utils/storage';
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string()
@@ -19,15 +23,37 @@ let attemptSignIn = (values) => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(values),
-      }).then(response => response.text())
-        .then(text => 
-          alert(text));  
+      }).then(res => res.json())
+        .then(json => { 
+        console.log('json', json); 
+        if(json.success) {
+          setInStorage('mail_on_bike', { token: json.token });
+        }
+    });  
 }
 
+let verify = () => {
+const obj = getFromStorage('mail_on_bike');
+  if (obj && obj.token) {
+    const { token } = obj;
+    // Verify token
+    fetch('http://localhost:9000/verify?token=' + token, {  headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }})
+      .then(res => res.json())
+      .then(json => {
+          console.log("worked and set the cokie "); 
+        });
+      } else {
+      console.log("failed to find a token")
+  }
+}
 
 const Login = () => (
   <div> 
-  <h1>this is a login page for people who have accounts aleady</h1> 
+  <h1>this is a login page for people who have accounts aleady</h1>
+  <button onClick={verify}> click to verify </button> 
   <Formik 
     initialValues={{
         email: ''
