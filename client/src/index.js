7469a87e80aca46fea2b96ca6906c24bf191323b
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-19 11:49:55 -0500

    properly displays the correct view!
    now just do it automatically? and create a logout

--- client/src/index.js
+++ client/src/index.js
@@ -2,57 +2,13 @@ import React from 'react';
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './pages/App';
-import Login from './pages/Login'; 
-import Signup from './pages/Signup'; 
-import { getFromStorage } from './utils/storage';
 
 const app = document.getElementById('root'); 
-
-function verify() {
-	const obj = getFromStorage('mail_on_bike');
-	if (obj && obj.token) {
-	const { token } = obj;
-	// Verify token
-	fetch('http://localhost:9000/verify?token=' + token)
-	.then(res => res.json())
-	.then(json => {
-  	if (json.success) {
-	    return userAccountView(); 
-	  } else {
-	  	return loginView(); 
-	  }
-	});
-	} else { return loginView(); }
-}
-
-
-const userAccountView = () => {
-	return (
-	<BrowserRouter>
-		<div> 
-	   		<App/>
-		</div> 
-   </BrowserRouter >
-	)
-}
-
-const loginView = () => {
-	return (
-	<BrowserRouter>
-		<div> 
-	   		<Login/>
-	   		<Signup/>
-		</div> 
-   </BrowserRouter>
-	)
-}
-
-// render(verify(), app); 
 
 render(
 	<BrowserRouter>
 		<div> 
 	   		<App/>
 		</div> 
-   </BrowserRouter> , app); 
+   </BrowserRouter>, app); 
 
--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,12 +1,57 @@
 import React from 'react'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import Login from './Login'; 
+import Signup from './Signup'; 
+import { getFromStorage } from '../utils/storage';
+	
+export default class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = { 
+			isLoggedIn: false 
+		}
+	}
 
-const App = () => (
-  <div>
-    <Header />
-    <Main />
-  </div>
-)
+	userAccountView = () => {
+		return (
+		  <div>
+		    <Header />
+		    <Main />
+		  </div>
+		)
+	}
 
-export default App; 
+	loginView = () => {
+		return (
+			<div> 
+		   		<Login/>
+		   		<Signup/>
+			</div> 
+		)
+	}
+
+	verify = () => {
+		const obj = getFromStorage('mail_on_bike');
+		if (obj && obj.token) {
+		const { token } = obj;
+		// Verify token
+		fetch('http://localhost:9000/verify?token=' + token)
+		.then(res => res.json())
+		.then(json => {
+	  	if (json.success) {
+		    this.setState({isLoggedIn: true}); 
+		  }
+		});
+		}	
+	}
+
+	componentDidMount() {
+		this.verify(); 
+	}
+
+	render() {
+		return this.state.isLoggedIn ?  this.userAccountView() : this.loginView()
+	}
+}
+
