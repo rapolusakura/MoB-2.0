518901017d6d98dbc1ce1d6a16fc2636a62f33db
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-26 01:00:51 -0500

    holy shit theres an autocomplete search bar

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import Autocomplete from 'react-google-autocomplete';
 
 const mapStyles = {
   width: '100%',
@@ -10,15 +11,30 @@ class MapContainer extends React.Component {
 
   render() {
     return (
+      <div>
+
+      <Autocomplete
+    style={{width: '90%'}}
+    onPlaceSelected={(place) => {
+      console.log(place);
+    }}
+    types={['(regions)']}
+    componentRestrictions={{country: "pe"}}
+      />
+
      <Map
           google={this.props.google}
           zoom={8}
           style={mapStyles}
           initialCenter={{ lat: 47.444, lng: -122.176}}
         />
+
+
+
+
+        </div>
       )
   }
-     
 }
 
 export default GoogleApiWrapper({
