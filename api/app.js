c8bd1a45abb57bbfaf29503fdefa8241aef0049f
Sakura Rapolu <rapolusakura@gmail.com>
2019-07-25 23:51:42 -0500

    used process.env

--- api/app.js
+++ api/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
 const bodyParser = require('body-parser');
-
 var indexRouter = require('./routes/index');
 var testAPIRouter = require('./routes/testAPI'); 
 
