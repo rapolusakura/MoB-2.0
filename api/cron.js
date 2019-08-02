9bdaee9934ab313b684f19a9e2ba9115c255fb0e
Sakura Rapolu <rapolusakura@gmail.com>
2019-08-02 10:49:35 -0500

    sends message of the available bikers

--- api/cron.js
+++ api/cron.js
@@ -2,7 +2,12 @@ const CronJob = require('cron').CronJob;
 const fetch = require("node-fetch");
 const AvailableBikers = require('./models/AvailableBikers')
 var db = require('./db.js'); 
-
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+const TWILIO_TEST_NUM = 'whatsapp:+14155238886'; 
+const TWILIO_PROD_NUM = 'whatsapp:+'
 
 shiftAvailableList = () => {
   AvailableBikers.find({}, function(err, record) {
@@ -13,25 +18,44 @@ shiftAvailableList = () => {
       let availableTomorrow = record[0].availableTomorrow; 
       AvailableBikers.updateOne({"tag":1}, {$set: {"availableToday": availableTomorrow, "availableTomorrow": []}}, function(err, success) {
         if(err) {console.log(err)} 
-          else {console.log("succesfully shifted lists")}
+          else {
+          	console.log("succesfully shifted lists")
+          	//message the list to anderson
+          	client.messages
+			  .create({
+			    from: TWILIO_TEST_NUM,
+			    body: `Hi Sakura, this is the list for people availableTomorrow: ${availableTomorrow}!`,
+			    to: `whatsapp:+18082037593`
+			  })
+          }
       })
   }  
 })
 }
 
-// notifyBikers = () => {
-// fetch("http://localhost:9000/notifyBikers")
-// }
+notifyBikers = () => {
+ fetch("http://localhost:9000/notifyBikersTest")
+}
 
-//00 02 17 * * 1-5
+//00 05 18 * * 0-4
 //the above will execute every 5:02 pm from Mon-Fri, what we want is sunday - thursday?? 
 console.log('Before job instantiation');
 
-const job = new CronJob('*/15 * * * * *', function() {
-shiftAvailableList(); 
-// notifyBikers(); 
+const shiftLists = new CronJob('*/15 * * * * *', function() {
+	shiftAvailableList(); 
 console.log("this happened just now oh my gosh!"); 
 }, 'America/Lima');
+
+
+
+	const job = new CronJob('*/15 * * * * *', function() {
+	shiftAvailableList(); 
+	// notifyBikers(); 
+	console.log("this happened just now oh my gosh!"); 
+	}, 'America/Lima');
+
+
+	
 console.log('After job instantiation');
 
-job.start();
+shiftLists.start();
