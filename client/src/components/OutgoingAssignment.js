b9aa317aa479f784400d4134508f00c94742d4b2
Sakura Rapolu <rapolusakura@gmail.com>
2019-08-03 10:40:33 -0500

    holy fuck you can assign bikers and it shows in the resopnse

--- client/src/components/OutgoingAssignment.js
+++ client/src/components/OutgoingAssignment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
 class OutgoingAssignment extends React.Component {
   constructor(props) {
@@ -14,20 +15,55 @@ class OutgoingAssignment extends React.Component {
           .then(res => this.setState({ 'availableBikers': res }));
   }
 
+  callAPI = (list) => {
+    fetch("http://localhost:9000/assignBikers", {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        list,
+      }),
+    }).then(res => res.json())
+        .then(json => { 
+        console.log('json', json); 
+        if(json.success) {
+          console.log("successfully assigned bikers!!");
+        }
+    });
+  }
+
   componentWillMount() {
       this.getBikers();
   }
 
   render() {
-    return (
-      <ul> 
-      {this.state.availableBikers.map( (biker, index) => {
-        return (
-            <h1> {biker.name} ({biker.num_current_orders})</h1> 
+    return (   
 
+          <Formik
+      onSubmit = { values => {
+          console.log('submitting', values);
+          //callAPI(values); 
+      }}
+    > 
+    <Form> 
+       <div> 
+     {this.state.availableBikers.map( (biker, index) => {
+        return (
+          <div> 
+          <label> 
+            <Field type="checkbox" name={biker._id}/> 
+            {biker.name} ({biker.num_current_orders})
+          </label> 
+          </div> 
           )
       })}
-      </ul> 
+     <button type="submit"> assign bikers </button> 
+     </div>
+    </Form>
+    </Formik> 
+     
     )
   }
 }
