86ff7bc78e90fe7e6a217d635644db88fb1e4609
Sakura Rapolu <rapolusakura@gmail.com>
2019-08-05 01:30:11 -0500

    heroku deployment works

--- api/app.js
+++ api/app.js
@@ -1,47 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
-const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
 var indexRouter = require('./routes/index');
 var testAPIRouter = require('./routes/testAPI'); 
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(cookieParser());
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors()); 
-
+app.set('view engine', 'jade');
 //route separation
 app.use('/', indexRouter);
 app.use('/testAPI', testAPIRouter); 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Put all API endpoints under '/api'
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  // Generate some passwords
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  )
+
+  // Return them as json
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/views/build.html'));
 });
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Password generator listening on ${port}`);
