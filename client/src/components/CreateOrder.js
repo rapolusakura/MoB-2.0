6ee48d52065fee5bc545714a57406bb7ae03142a
Sakura Rapolu <rapolusakura@gmail.com>
2019-08-13 09:31:51 -0500

    correctly displays rate and distance

--- client/src/components/CreateOrder.js
+++ client/src/components/CreateOrder.js
@@ -47,109 +47,111 @@ export default class orderForm extends React.Component {
     }); 
   }
 
-  CreateOrderSchema = () => { 
-    Yup.object().shape({
-    companyName: Yup.string()
-      .required('Company name is required'),
-    destContact: Yup.string()
-      .required('the contact name of the destination is required'),
-    destPhone: Yup.number()
-      .positive()
-      .required('the phone number of the dest is required'), 
-    method_of_payment: Yup.string()
-      .required('method of payment is required'),
-  })};
+CreateOrderSchema = () => { 
+  Yup.object().shape({
+  companyName: Yup.string()
+    .required('Company name is required'),
+  destContact: Yup.string()
+    .required('the contact name of the destination is required'),
+  destPhone: Yup.number()
+    .positive()
+    .required('the phone number of the dest is required'), 
+  method_of_payment: Yup.string()
+    .required('method of payment is required'),
+})};
 
  createOrderAPI = (values) => {
   let mode = ''; 
   values.mode ? mode = 'round-trip' : mode = 'one-way'; 
   console.log("starting addy", this.state.startingAddress)
   console.log("dest addy", this.state.destAddress)
-  fetch("/createOrder", {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        //i mean like ideally the company will be a part of the user.... like it literally alrady is through the ID so they shouldnt
-        //have to input it in the form.. when they get approved (will also need to implement this verification process), just query the companies 
-        //db with the company id and grab the official company name?? or the razon commercial, RUC (if it exists), type of rate
-        client_company_id : this.state.employer, 
-        client_company_name : values.companyName,
-        special_instructions : "Origin Notes:\n".concat(values.origin_notes).concat('\nDestination Notes:\n').concat(values.dest_notes), 
-        type_of_load : values.type_of_load, 
-        mode : mode, 
-        distance : this.state.distance,
-        rate : this.state.rate, 
-        client_address : this.state.startingAddress, 
-        dest_address : this.state.destAddress, 
-        dest_contact_name : values.destContact, 
-        dest_company_name: values.destCompany,
-        dest_phone_number: values.destPhone, 
-        startLat: this.state.startingLat, 
-        startLng: this.state.startingLng, 
-        endLat: this.state.destLat,
-        endLng: this.state.destLng
-      }),
-    }).then(res => res.json())
-        .then(json => { 
-        console.log('json', json); 
-        if(json.success) {
-          console.log("worked!!");
-        }
-    });
+    fetch("/createOrder", {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          //i mean like ideally the company will be a part of the user.... like it literally alrady is through the ID so they shouldnt
+          //have to input it in the form.. when they get approved (will also need to implement this verification process), just query the companies 
+          //db with the company id and grab the official company name?? or the razon commercial, RUC (if it exists), type of rate
+          client_company_id : this.state.employer, 
+          client_company_name : values.companyName,
+          special_instructions : "Origin Notes:\n".concat(values.origin_notes).concat('\nDestination Notes:\n').concat(values.dest_notes), 
+          type_of_load : values.type_of_load, 
+          mode : mode, 
+          distance : this.state.distance,
+          rate : this.state.rate, 
+          client_address : this.state.startingAddress, 
+          dest_address : this.state.destAddress, 
+          dest_contact_name : values.destContact, 
+          dest_company_name: values.destCompany,
+          dest_phone_number: values.destPhone, 
+          startLat: this.state.startingLat, 
+          startLng: this.state.startingLng, 
+          endLat: this.state.destLat,
+          endLng: this.state.destLng
+        }),
+      }).then(res => res.json())
+          .then(json => { 
+          console.log('json', json); 
+          if(json.success) {
+            console.log("worked!!");
+          }
+      });
 }
 
-  calculateRate = (isRoundTrip) => {
-    if(this.state.startingPlaceId != '' && this.state.destPlaceId != '') {
-      let mode = ''; 
-      isRoundTrip ? mode = 'round-trip' : mode = 'one-way'; 
+calculateRate = (isRoundTrip, moneyCollection) => {
+  if(this.state.startingPlaceId != '' && this.state.destPlaceId != '') {
+    let mode = ''; 
+    isRoundTrip ? mode = 'round-trip' : mode = 'one-way'; 
 
-      fetch("/calculateDistance", {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        start: this.state.startingPlaceId,
-        end: this.state.destPlaceId,
-        mode: mode
-      }),
-    }).then(res => res.json())
-        .then(json => { 
-          this.setState({distance: json.distance})
-          console.log('this is the distance', json.distance); 
-          if(this.state.employer != null) {
-              fetch("/calculateRate", {
-              method: 'POST',
-              headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                distance: json.distance,
-                companyId: this.state.employer
-              }),
-            })
-              .then(res => res.json())
-              .then(json => { 
-              if(json.success) {
-                console.log('this is the response from calculate rate: ', json)
-                this.setState({
-                  rate: json.rate, 
-                  type_of_rate: json.type_of_rate, 
-                  RUC: json.RUC, 
-                  client_company_name: json.client_company_name
-                }); 
-              }
-              });
-          }
-        });
-        //3) on the UI side, if they are an admin, render a component that allows them to search a company by their RUC or razon commercial
-    }
+    fetch("/calculateDistance", {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      start: this.state.startingPlaceId,
+      end: this.state.destPlaceId,
+      mode: mode
+    }),
+  }).then(res => res.json())
+      .then(json => { 
+        this.setState({distance: json.distance})
+        console.log('this is the distance', json.distance); 
+        if(this.state.employer != null) {
+            fetch("/calculateRate", {
+            method: 'POST',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              distance: json.distance,
+              companyId: this.state.employer
+            }),
+          })
+            .then(res => res.json())
+            .then(json => { 
+            if(json.success) {
+              console.log('this is the response from calculate rate: ', json)
+              let realRate = -1;
+              if (moneyCollection == '' || moneyCollection == 0 || moneyCollection == null || moneyCollection == 'undefined') { realRate = json.rate } else { realRate = json.rate + 2;}
+              this.setState({
+                rate: realRate, 
+                type_of_rate: json.type_of_rate, 
+                RUC: json.RUC, 
+                client_company_name: json.client_company_name
+              }); 
+            }
+            });
+        }
+      });
+      //3) on the UI side, if they are an admin, render a component that allows them to search a company by their RUC or razon commercial
   }
+}
 
 updateAddress = (isOrigin, address, place_id, lat, lng) => {
   if(isOrigin) {
@@ -171,7 +173,16 @@ updateAddress = (isOrigin, address, place_id, lat, lng) => {
 }
 
   render() {
+
+    let apiLabel;
+
+    if (this.state.rate != -1 && this.state.distance != -1) {
+      apiLabel = <label> Distance: {this.state.distance} Rate: {this.state.rate} </label>;
+    } else {
+      apiLabel = <label> Distance: N/A Rate: N/A </label>;
+    }
     return (
+      <div>
           <Formik 
            initialValues={{
             companyName: '', 
@@ -198,18 +209,19 @@ updateAddress = (isOrigin, address, place_id, lat, lng) => {
             Round-trip delivery?
             </label> 
             <br/>
+
             <label> Method of Payment </label> 
             <select
               name="method_of_payment"
               value={values.method_of_payment}
               style={{ display: 'block' }}
             >
-              <option value="" label="Select a method of payment" />
               <option label="Cash on origin" value="cash_on_origin" />
               <option label="Cash on destination" value="cash_on_destination" />
               <option label="Bank transfer" value="bank_transfer" />
             </select>
             {errors.method_of_payment && touched.method_of_payment ? <div>{errors.method_of_payment}</div> : null}
+            <Field name="money_collection" type="text" placeholder = "Recaudo" /> 
 
             <br/>
 
@@ -248,15 +260,15 @@ updateAddress = (isOrigin, address, place_id, lat, lng) => {
             />
 
         </div>
-            <button style={{margin: '40px'}} type = "button" onClick={() => {this.calculateRate(values.mode)}}> Calculate Rate </button>
+          {apiLabel}
+            <button style={{margin: '40px'}} type = "button" onClick={() => {this.calculateRate(values.mode, values.money_collection)}}> Calculate Rate </button>
             <button type="submit"> Submit </button>
           </div> 
       </Form>
       )}
     </Formik>
-
+    </div>
     )
   }
-
 
 }
